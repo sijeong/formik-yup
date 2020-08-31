a30da9dadc1395da85da404c14454a4440623545
sijeong <sijeong@outlook.com>
2020-09-01 01:39:44 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import { Router, Link, Route } from 'react-router-dom';
 import './App.css';
 import ValidatedLoginForm from './Form';
 import { IndexPage, AboutPage, UsersPage, UserPage } from './test';
+import { Form } from 'formik';
 
 const history = createBrowserHistory();
 function App() {
   return (
     <div className="">
       <header className="">
-        <ValidatedLoginForm />
+        {/* <ValidatedLoginForm /> */}
         {/* <Register /> */}
       </header>
       <Router history={history}>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/users/">Users</Link>
+        <Link to="/form">Form</Link>
         <Route exact path="/" component={IndexPage} />
+        <Route path="/form" component={ValidatedLoginForm} />
         <Route exact path="/users" component={UsersPage} />
         <Route exact path="/user/:userId" component={UserPage} />
         <Route path="/about" component={AboutPage} />
--- src/Form.tsx
+++ src/Form.tsx
@@ -3,6 +3,14 @@ import React, { useEffect } from 'react';
 import './Form.css';
 import { InputTag } from './InputTag';
 import { validationSchema } from './model';
+// @ts-ignore
+import CKEditor from '@ckeditor/ckeditor5-react';
+// @ts-ignore
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// @ts-ignore
+import BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
+// @ts-ignore
+import sanitize from 'sanitize-html';
 
 const ValidatedLoginForm = () => {
   const {
@@ -13,6 +21,7 @@ const ValidatedLoginForm = () => {
     handleChange,
     handleBlur,
     handleSubmit,
+    handleReset,
     isValid,
     validateForm,
   } = useFormik({
@@ -24,6 +33,38 @@ const ValidatedLoginForm = () => {
     (() => validateForm())();
   }, [validateForm]);
 
+  const htmlContent = `<p>Hello every....</p>`;
+  const anotherContent = `<img href='asdf' />`;
+  const longContent = `<div class="headline">
+  <h1>StudyBass Fundamentals One</h1>
+</div>
+<div class="curr-header">
+  <div class="curr-header-img">
+  <image src="https://b570f299d8a96ff32c59-2e725a51007a5cce3182c57bd45640f2.ssl.cf1.rackcdn.com/images/SBFI-large.png" />
+  </div>
+                                                                                                          <h2>Curriculum Overview</h2>
+  <h3 id="lesson-link">8 Blocks | 56 Lessons</h3>
+  <p>Welcome new StudyBasser! In this first curriculum my intention is to put you on the right track to be <i>not a bass player</i>, but a <u><b>bass-playing musician</b></u>. The way you <i>think</i> about music and learning music has a big effect on how fast and well it goes for you. This curriculum gets you thinking right. The lessons start with a fair amount of important reading. Then we start playing!</p>
+<div style="clear: both;">
+<h4>In this StudyBass curriculum you will learn:</h4>
+<ul>
+ <li>A broad overview of music</li>
+ <li>The role of the bass</li>
+ <li>Essential beginning music theory</li>
+ <li>Basic bass technique</li>
+ <li>Reading music notation, bass tab and StudyBass AlphaTab</li>
+ <li>Tips on practicing</li>
+ <li>Simple bass note patterns used in most basslines</li>
+ <li>Essential basic rhythm skills</li>
+ <li>Beginning intervals</li>
+</ul>
+</div>`;
+  console.log(
+    `here is result: ${sanitize(longContent, {
+      allowedTags: [],
+      allowedAttributes: {},
+    })}`
+  );
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="email">Email</label>
@@ -60,6 +101,18 @@ const ValidatedLoginForm = () => {
         value={values.memo}
         onChange={handleChange}
         onBlur={handleBlur}></textarea>
+      <CKEditor
+        editor={BalloonEditor}
+        data={longContent}
+        onInit={(editor: any) => {}}
+        disabled="true"
+        // config={
+        //   {
+       
+        //     isReadOnly: true,
+        //   }
+        // }
+      />
       <InputTag />
       {/* <ReactTags handleAddition={() => handleAddition} handleDelete={() => handleDelete} /> */}
       <button type="submit" disabled={isSubmitting || !isValid}>
